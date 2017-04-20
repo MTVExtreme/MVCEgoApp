ba81f61fe0c1fb7782a84629cb0e9f41e0695a4f
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-20 16:02:41 -0400

    Added and Adjusted a Navbar Template

--- PersonalityEgo/App_Start/BundleConfig.cs
+++ PersonalityEgo/App_Start/BundleConfig.cs
@@ -17,7 +17,7 @@ namespace PersonalityEgo
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -25,7 +25,13 @@ namespace PersonalityEgo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/reset.css",
+                      "~/Content/style.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                     "~/Scripts/main*"));
+
+
         }
     }
 }
